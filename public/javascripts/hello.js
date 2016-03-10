bde46b97098e93bd5426700285ec00bcfa1ceb90
jslucassf <jslucassf@gmail.com>
2016-03-10 17:32:43 -0300

    Driver's page created,the cookie is a lie :)

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -1,3 +1,12 @@
 if (window.console) {
   console.log("Welcome to your Play application's JavaScript!");
 }
+
+
+function initMap() {
+  var mapDiv = document.getElementById('map');
+  var map = new google.maps.Map(mapDiv, {
+    center: {lat: -7.2148147, lng: -35.9087772},
+    zoom: 15
+  });
+}
