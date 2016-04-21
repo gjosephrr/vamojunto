2728f5ea498b41f1373f920136dea87c3c8cad20
RicardoMagno <magnoricardo7@gmail.com>
2016-04-21 15:54:17 -0300

    Pendind Requests

--- view_service/app/api_service/DataAccessLayer/SolicitationDAO.java
+++ view_service/app/api_service/DataAccessLayer/SolicitationDAO.java
@@ -1,10 +1,33 @@
 package api_service.DataAccessLayer;
 
+import api_service.models.Solicitation;
+import java.util.ArrayList;
+
 /**
  * Created by joseph on 20/04/16.
  */
 public class SolicitationDAO {
 
+    private ArrayList<Solicitation> solicitationsDB;
 
+    public SolicitationDAO(){
+        solicitationsDB = new ArrayList<Solicitation>();
+    }
+
+    public void registerSolicitation(String requester_id,String responser_id, String ride_id){
+        // Instantiate new solicitation
+        Solicitation solicitation = new Solicitation(requester_id,responser_id, ride_id);
+        solicitationsDB.add(solicitation);
+
+    }
+    public ArrayList<Solicitation> listPendingRequest(String responser_id) {
+        ArrayList<Solicitation> pendingRequests = new ArrayList<Solicitation>();
+        for (Solicitation solicitation : solicitationsDB) {
+            if (solicitation.isSimilarDriver(responser_id)) {
+                pendingRequests.add(solicitation);
+            }
+        }
+        return pendingRequests;
+    }
 
 }
