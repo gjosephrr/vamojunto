be3757f4f8c6bca219515215090d776e93827030
RicardoMagno <magnoricardo7@gmail.com>
2016-04-21 09:55:00 -0300

    Creat Solicitacion

--- view_service/app/api_service/models/Solicitation.java
+++ view_service/app/api_service/models/Solicitation.java
@@ -5,19 +5,30 @@ package api_service.models;
  */
 public class Solicitation {
 
-    private enum status{
-        ACCEPTED, REFUSED;
+    private enum Status{
+        ACCEPTED, REFUSED, WAITING;
     }
 
     private String requester_id;
     private String responser_id;
     private String ride_id;
-    private String status;
+    private Status status;
 
     public Solicitation(String requester_id,String responser_id, String ride_id){
-
-
-
+        this.requester_id = requester_id;
+        this.responser_id = responser_id;
+        this.ride_id = ride_id;
+        this.status = Status.WAITING;
     }
 
+    public String getPassenger() {
+        return requester_id;
+    }
+    public void setStatus(boolean newStatus){
+        if(newStatus){
+            this.status = status.ACCEPTED;
+        }else{
+            this.status = status.REFUSED;
+        }
+    }
 }
